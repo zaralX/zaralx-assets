5928d1719d67f622a0451dc4816bad2cb6e9de7d
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-01 02:01:09 +0800

    updated info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 under development
+
+https://assets.zaralx.ru/api/v1/minecraft/vanilla/item/stone
--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,14 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
+    fastify.get('/', async function (request, reply) {
+        return {
+            message: "Open source project. Github: https://github.com/zaralX/zaralx-assets",
+            sources: {
+                vanilla: {
+                    version: "1.21.5"
+                }
+            }
+        }
+    })
 }
