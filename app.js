1303f7904cd67fe7a595c417c3ffec6159bf4d5d
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-01 03:08:59 +0800

    lang support

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
-  fastify.register(AutoLoad, {
+  await fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
   })
 
   // This loads all plugins defined in routes
   // define your routes in one of these
-  fastify.register(AutoLoad, {
+  await fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
   })
