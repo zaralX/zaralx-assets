2aee4ff967a1dca85933790b6ee3bbc10d15e2b9
zaralX <looppersmail@gmail.com>
2026-01-06 21:43:02 +0800

    Nickname util added redis

--- src/utils/nicknameUtil.ts
+++ src/utils/nicknameUtil.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import {FastifyInstance} from "fastify";
 
 export function isValidMinecraftNickname(nickname: string) {
     const regex = /^[a-zA-Z0-9_]{3,16}$/;
     return regex.test(nickname);
 }
 
-export async function nicknameToUUID(nickname: string) {
+export async function nicknameToUUID(fastify: FastifyInstance | null, nickname: string) {
+    if (fastify) {
+        const result = await fastify.redis.get(`minecraft_vanilla_nickname:${nickname}`)
+        if (result) {
+            return result
+        }
+    }
     const res = await axios.get(
         `https://api.mojang.com/users/profiles/minecraft/${nickname}`,
         { validateStatus: s => s === 200 || s === 204 }
@@ -13,6 +20,10 @@ export async function nicknameToUUID(nickname: string) {
 
     if (res.status === 204) {
         throw new Error('Player not found')
+    }
+
+    if (fastify) {
+        fastify.redis.set(`minecraft_vanilla_nickname:${nickname}`, res.data.id)
     }
 
     return res.data.id
